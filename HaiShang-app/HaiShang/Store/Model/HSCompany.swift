5e43425520aff71f552a311535d0646ba4a19af1
mithvv <mithvv@gmail.com>
2015-03-21 00:40:29 +0800

    add HSCompany

---
+++ HaiShang-app/HaiShang/Store/Model/HSCompany.swift
@@ -0,0 +1,15 @@
+//
+//  HSCompany.swift
+//  HaiShang
+//
+//  Created by mAir on 21/3/15.
+//  Copyright (c) 2015 mithvv. All rights reserved.
+//
+
+
+
+class HSCompany: NSObject {
+
+    
+    var name            : String?
+}
---
+++ HaiShang-app/HaiShang/Store/Model/HSProjectCompanys.swift
@@ -0,0 +1,15 @@
+//
+//  HSProjectCompanys.swift
+//  HaiShang
+//
+//  Created by mAir on 21/3/15.
+//  Copyright (c) 2015 mithvv. All rights reserved.
+//
+
+
+
+class HSProjectCompanys: NSObject {
+
+    var project     : HSProject?
+    var company     : HSCompany?
+}
